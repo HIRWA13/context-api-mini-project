950a8c9985bde9d975381ad2ed04445e336b6663
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-04 17:30:32 +0200

    feat: add the Result component

---
+++ src/components/Result.jsx
@@ -0,0 +1,11 @@
+import "./Result.css";
+
+export default function Result() {
+  return (
+    <div>
+        <h1 className="result"><span>- -</span> years</h1>
+        <h1 className="result"><span>- -</span> months</h1>
+        <h1 className="result"><span>- -</span> days</h1>
+    </div>
+  )
+}

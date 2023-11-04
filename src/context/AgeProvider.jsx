652ec6f03710fe6f8ecf83961b532618559ad156
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-04 17:31:55 +0200

    feat: add context provider

---
+++ src/context/AgeProvider.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react"
+import { AgeContext } from "./AgeContext"
+
+export default function AgeProvider() {
+    const [age, setAge] = useState({})
+  return (
+    <div></div>
+  )
+}

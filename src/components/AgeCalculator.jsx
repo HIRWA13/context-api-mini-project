1d28fb5fe54c8ad4304c4a417fef84cd1c4612b3
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-04 17:29:03 +0200

    feat: add the AgeCalculator component

---
+++ src/components/AgeCalculator.jsx
@@ -0,0 +1,12 @@
+import Form from "./Form";
+import "./AgeCalculator.css";
+import Result from "./Result";
+
+export default function AgeCalculator() {
+  return (
+    <div className="container">
+      <Form />
+      <Result />
+    </div>
+  );
+}

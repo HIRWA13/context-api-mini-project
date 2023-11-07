dd03e0fafbae7076f9e253e06c0f942342163c02
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-07 08:51:46 +0200

    feat: add context

--- src/components/AgeCalculator.jsx
+++ src/components/AgeCalculator.jsx
@@ -1,12 +1,21 @@
 import Form from "./Form";
 import "./AgeCalculator.css";
 import Result from "./Result";
+import { AgeContext } from "../context/AgeContext";
+import { useState } from "react";
 
 export default function AgeCalculator() {
+  const [age, setAge] = useState({
+    day: null,
+    month: null,
+    year: null,
+  });
   return (
-    <div className="container">
-      <Form />
-      <Result />
-    </div>
+    <AgeContext.Provider value={{age, setAge}}>
+      <div className="container">
+        <Form />
+        <Result />
+      </div>
+    </AgeContext.Provider>
   );
 }

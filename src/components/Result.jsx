1ebb58cae4b3968ef92999abad8f39244235b8f0
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-07 08:52:51 +0200

    feat: display data from the context

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,21 @@
 import "./Result.css";
+import { useContext } from "react";
+import { AgeContext } from "../context/AgeContext";
 
 export default function Result() {
+  const {age, setAge} = useContext(AgeContext)
+  console.log(age)
   return (
     <div>
-        <h1 className="result"><span>- -</span> years</h1>
-        <h1 className="result"><span>- -</span> months</h1>
-        <h1 className="result"><span>- -</span> days</h1>
+      <h1 className="result">
+        <span>{age.day ? age.day : "- -"}</span> Days
+      </h1>
+      <h1 className="result">
+        <span>{age.month ? age.month : "- -"}</span> Months
+      </h1>
+      <h1 className="result">
+        <span>{age.year ? age.year : "- -"}</span> Years
+      </h1>
     </div>
-  )
+  );
 }

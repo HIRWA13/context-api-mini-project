2f91910011ca1462100e9fdc71ed9c3f56194307
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-07 08:52:13 +0200

    feat: introduce the useRef hook to get input value

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,60 @@
 import "./Form.css";
 import iconarrow from "../assets/iconarrow.svg";
+import { useContext, useRef } from "react";
+import { AgeContext } from "../context/AgeContext";
 
 export default function Form() {
+  const {age, setAge} = useContext(AgeContext);
+  const dayInputRef = useRef(age.name);
+  const monthInputRef = useRef(age.month);
+  const yearInputRef = useRef(age.year);
+
+  const AgeCalculatorHandler = (e) => {
+    e.preventDefault();
+    setAge(()=> {
+      return {
+        day: dayInputRef.current.value,
+        month: monthInputRef.current.value,
+        year: yearInputRef.current.value
+      }
+    })
+    dayInputRef.current.value = ''
+    monthInputRef.current.value = ''
+    yearInputRef.current.value = ''
+  }
+
   return (
-    <form className="age-form">
+    <form className="age-form" onSubmit={AgeCalculatorHandler}>
       <div className="inputs-container">
         <div className="inputs">
           <label htmlFor="name">Day</label>
-          <input type="number" id="name"min={0} placeholder="DD" required/>
+          <input 
+              type="number" 
+              id="name"
+              min={0} 
+              placeholder="DD"
+              ref={dayInputRef}
+              />
         </div>
         <div className="inputs">
           <label htmlFor="name">Month</label>
-          <input type="number" id="name"min={0} placeholder="MM" required/>
+          <input 
+              type="number" 
+              id="name"
+              min={0} 
+              placeholder="MM"
+              ref={monthInputRef}
+              />
         </div>
         <div className="inputs">
           <label htmlFor="name">Year</label>
-          <input type="number" id="name"min={0} placeholder="YYYY" required/>
+          <input 
+              type="number" 
+              id="name"
+              min={0} 
+              placeholder="YYYY"
+              ref={yearInputRef}
+              />
         </div>
       </div>
       <div>

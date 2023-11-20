b71bd24ffb50f6bfc865747c63c56c6b7e0fdbcc
Junior Hirwa <iamhirwejr@gmail.com>
2023-11-20 17:36:59 +0200

    chore: remove AgeProvider

--- src/context/AgeProvider.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react"
-import { AgeContext } from "./AgeContext"
-
-export default function AgeProvider() {
-    const [age, setAge] = useState({})
-  return (
-    <div></div>
-  )
-}
